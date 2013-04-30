6a75bbba1da0e8ccbc03a6a421b4ade06d1b3dda
Evan Simpson <esimpson.rescue@gmail.com>
2013-04-29 21:12:50 -0400

    Added start-duration constructor to DateTime

--- src/com/example/calendarapp/Event.java
+++ src/com/example/calendarapp/Event.java
@@ -2,6 +2,8 @@ package com.example.calendarapp;
 
 import java.io.Serializable;
 import java.text.ParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Event implements Serializable {
 	private static final long serialVersionUID = -5185389970304553876L;
@@ -20,6 +22,49 @@ public class Event implements Serializable {
 		this.start = new DateTime(start);
 		this.end = new DateTime(end);
 		setDescription(description);
+	}
+	
+	public Event(Long start, Long end, String description){
+		this.start = new DateTime(start);
+		this.end = new DateTime(end);
+		setDescription(description);
+	}
+	
+	public Event(Long start, String duration, String description){
+		this.start = new DateTime(start);
+		this.end = new DateTime(endFromDuration(start, duration));
+		setDescription(description);
+	}
+	
+	public Long endFromDuration(Long start, String duration){
+		Long nd = start;
+		Pattern pattern = Pattern.compile("([0-9]*[D|H|M|S])");
+		Matcher matcher = pattern.matcher(duration);
+		while (matcher.find()) {
+            String group = matcher.group();
+            if (group.charAt(group.length()-1) == 'D'){
+            	String numbers = group.substring(0, group.length()-1);
+            	int millis = Integer.parseInt(numbers);
+            	millis = millis * 86400000;
+            	nd = nd + millis;
+            }else if (group.charAt(group.length()-1) == 'H'){
+            	String numbers = group.substring(0, group.length()-1);
+            	int millis = Integer.parseInt(numbers);
+            	millis = millis * 3600000;
+            	nd = nd + millis;
+            }else if (group.charAt(group.length()-1) == 'M'){
+            	String numbers = group.substring(0, group.length()-1);
+            	int millis = Integer.parseInt(numbers);
+            	millis = millis * 60000;
+            	nd = nd + millis;
+            }else if (group.charAt(group.length()-1) == 'S'){
+            	String numbers = group.substring(0, group.length()-1);
+            	int millis = Integer.parseInt(numbers);
+            	millis = millis * 1000;
+            	nd = nd + millis;
+            }
+        }
+		return nd;
 	}
 	
 	public DateTime getStart() {
